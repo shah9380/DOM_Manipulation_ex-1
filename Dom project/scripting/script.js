99381ff76dca5b356830c62a9628256fdfe62c16
shah9380 <130676464+shah9380@users.noreply.github.com>
2023-11-01 15:58:52 +0530

    Add files via upload

---
+++ Dom project/scripting/script.js
@@ -0,0 +1,31 @@
+// Retrieve the target div element by its ID
+const targetDiv = document.getElementById("ChangeMe");
+
+// Change the background color to yellow
+targetDiv.style.backgroundColor = "yellow";
+
+//change the margin to 20px from all sides
+targetDiv.style.margin = "20px";
+
+// Change the padding to 10px
+targetDiv.style.padding = "10px";
+
+//Change te font size to 18px
+targetDiv.style.fontSize = '18px';
+
+//Change the font weight to bold 
+targetDiv.style.fontWeight = "bold";
+
+//Change the height of the element to 200px
+targetDiv.style.height = "200px";
+
+//Change  the width of the element to 300px
+targetDiv.style.width = " 300px";
+
+targetDiv.style.display = "flex";
+
+targetDiv.style.justifyContent = "center";
+
+targetDiv.style.alignItems = "center";
+
+targetDiv.style.color = "blue";
